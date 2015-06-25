5feb9d64c7c2072a49a7eb8467851f762df54b0d
Matt Ray <mattray.apex@gmail.com>
2015-06-25 12:33:30 -0600

    first commit after a long break. all structure is done. last refinement needed.

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -18,7 +18,7 @@ $(document).ready(function(){
           target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
           if (target.length) {
             $('html,body').animate({
-              scrollTop: target.offset().top-70
+              scrollTop: target.offset().top-50
             }, 1000);
             return false;
           }
