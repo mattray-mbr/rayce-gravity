a7a6cc92d0d99232831165d0219559c62950e6ef
mattray-mbr <mattray.mbr@gmail.com>
2016-04-04 19:48:01 -0600

    added server, trying to switch hosts to repost online

---
+++ server.js
@@ -0,0 +1,31 @@
+// initial setup
+var express = require('express');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+
+
+
+
+var app = express();
+
+// Application Configuration, not really sure what these do
+app.use(logger('dev')); //no idea what this does
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/builds/production/'));  
+
+
+//--------------- Routes ---------------  
+//initial setup route
+app.get('/', function(req, res) {
+    res.sendFile('index.html', {root : './builds/production/'});
+});
+
+
+// Creating Server and Listening for Connections 
+var port = 3100
+app.listen(port, function(){
+  console.log('Server running on port ' + port);
+
+})
